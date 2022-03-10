af145f64a68c615c53593a74e274e58bbbd41af2
cariahmarey <74411053+cariahmarey@users.noreply.github.com>
2022-03-10 23:12:11 +0100

    Update Preprocessing.R

--- Preprocessing.R
+++ Preprocessing.R
@@ -17,7 +17,7 @@ library(htmltools)
 library(topicdoc)
 
 # path for dataset
-path_dataset <- paste("C:/Users/mariu/OneDrive/Dokumente/Studium Leipzig/Wintersemester 21-22/Bachelorarbeit/Topic Model Test in RStudio/STM Test BA/corpus_englisch_date.csv")
+path_dataset <- paste(<filepath>)
              
 # read csv
 textdata <- read.csv(path_dataset, header= T, sep = ";", encoding = "UTF-8")        
@@ -25,49 +25,41 @@ textdata <- read.csv(path_dataset, header= T, sep = ";", encoding = "UTF-8")
 # change column name
 colnames(textdata)[colnames(textdata) == "X.U.FEFF.File"] <- "Filenames"
 
-# dimensions of dataframe (1248 documents)
+# dimensions of dataframe
 dim(textdata)
 
 # detect duplicated cells -> delete in document
 duplicated(textdata)
 
-# new dimensions of dataframe (1232 documents)
+# new dimensions of dataframe
 dim(textdata)
 
 # create corpus
 textdata_corpus <- corpus(textdata$Text, docnames = textdata$Filenames)
 
-# calculate corpus tokens (37100 tokens)
+# calculate corpus tokens
 corpus_tokens <- ntoken(textdata_corpus)
 corpus_tokens_sum <- sum(data.frame(corpus_tokens))
 
-
-# calculate corpus types (5549 types) 
+# calculate corpus types
 DTM <- dfm(textdata_corpus)
 corpus_types <- dim(DTM)[2]
 
-# calculate type/token ratio (14,95687)
-type_token_ratio <- (corpus_types/corpus_tokens_sum)*100
-
 # import dictionary of lemmas
-lemma_data <- read.csv("C:/Users/mariu/OneDrive/Dokumente/Studium Leipzig/Wintersemester 21-22/Bachelorarbeit\\Topic Model Test in RStudio\\Stopwords\\baseform_en.txt", encoding = "UTF-8")
+lemma_data <- read.csv(<filepath>, encoding = "UTF-8")
 
-# remove lemmas data, datum and electronic-data, electronic-datum
+# remove lemmas "data, datum" and "electronic-data, electronic-datum"
 lemma_data <- lemma_data[-c(10322,13618), ]
 
 # import stopword list
-stopword_data <- readLines("C:/Users/mariu/OneDrive/Dokumente/Studium Leipzig/Wintersemester 21-22/Bachelorarbeit\\Topic Model Test in RStudio\\Stopwords\\stopwords_en.txt", encoding = "UTF-8")
+stopword_data <- readLines(<filepath>, encoding = "UTF-8")
 
-# create table with tokens
-# remove punctuation, numbers and symbols
-# transform tokens to only lowercase
-# remove stopwords
-# lemmatization
+# create tokens object
 corpus_tokens <- textdata_corpus %>%
-  tokens(remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE) %>% 
-  tokens_tolower() %>%
-  tokens_replace(lemma_data$inflected_form, lemma_data$lemma, valuetype = "fixed") %>%
-  tokens_remove(pattern = stopword_data, padding = T)
+  tokens(remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE) %>% # remove punctuation, numbers and symbols
+  tokens_tolower() %>% # transform tokens to only lowercase
+  tokens_replace(lemma_data$inflected_form, lemma_data$lemma, valuetype = "fixed") %>% # lemmatization
+  tokens_remove(pattern = stopword_data, padding = T) # remove stopwords
 
 # find collocations
 textdata_collocations <- textstat_collocations(corpus_tokens, min_count = 2)
@@ -75,20 +67,18 @@ textdata_collocations <- textstat_collocations(corpus_tokens, min_count = 2)
 textdata_collocations_sorted <- textdata_collocations[order(-textdata_collocations$count), ]
 # select all rows of sorted collocations
 textdata_collocations <- textdata_collocations_sorted[1:273, ]
-
+# insert collocations
 corpus_tokens <- tokens_compound(corpus_tokens, textdata_collocations)
 
 # create DTM
-# remove corpus-specific tokens
-# pruning: remove terms that appear less than 3 documents
 DTM <- corpus_tokens %>% 
-  tokens_remove(c("", "big", "data", "big_data")) %>%
+  tokens_remove(c("", "big", "data", "big_data")) %>% # remove corpus-specific tokens
   dfm() %>% 
-  dfm_trim(min_docfreq = 3, docfreq_type = "count")
+  dfm_trim(min_docfreq = 3, docfreq_type = "count") # pruning: remove terms that appear in less than 3 documents
 
 # remove empty rows of DTM
 sel_idx <- rowSums(DTM) > 0
 DTM <- DTM[sel_idx, ]
 
-# count tokens (8429 tokens)
+# count tokens
 DTM_tokens <- sum(ntoken(DTM))
