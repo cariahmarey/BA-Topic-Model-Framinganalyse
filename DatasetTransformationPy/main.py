128ef0035de1765d21f16b76292b82593ef2a32c
cariahmarey <74411053+cariahmarey@users.noreply.github.com>
2022-03-11 14:52:35 +0100

    Update main.py

--- DatasetTransformationPy/main.py
+++ DatasetTransformationPy/main.py
@@ -4,7 +4,20 @@ import csv
 import pandas as pd
 
 
-
+# if __name__ == '__main__':
+#    docxpath = "Docx\DocxEnglisch"
+#    doc_list = os.listdir(docxpath)
+#
+#    for doc in doc_list:
+#        path = os.path.join(docxpath, doc)
+#        document = Document(path)
+#        i = 0
+#        for p in document.paragraphs[1:]:
+#            if not len(l.text) == 0 and 'S.' in p.text and len(p.text) > 40:
+#                i += 1
+#                print(f'{doc} {p.text}')
+                
+###########################
 if __name__ == '__main__':
 
     # Pfad
@@ -17,28 +30,16 @@ if __name__ == '__main__':
         path = os.path.join(docxpath, doc)
         document = Document(path)
         for p in document.paragraphs[1:]:
-            if not len(p.text.strip()) == 0 and 'S.' not in p.text:
+            if not len(p.text.strip()) == 0 and 'S.' not in p.text: #search for 'S.' and linebreak
                 text = p.text.replace("\n", " ")
                 output_list.append([doc, text])
 
-    # Dataframe erstellen
+    # create dataframe
     output_df = pd.DataFrame(output_list, columns=['File', 'Text'])
-    # CSV aus Dataframe erstellen
+    # create csv from dataframe
     output_df.to_csv('corpus_englisch.csv',
                      index=False,
                      sep=';',
                      encoding='utf-8-sig')
 
-##################################
-if __name__ == '__main__':
-    docxpath = "Docx\DocxEnglisch"
-    doc_list = os.listdir(docxpath)
 
-    for doc in doc_list:
-        path = os.path.join(docxpath, doc)
-        document = Document(path)
-        i = 0
-        for p in document.paragraphs[1:]:
-            if not len(l.text) == 0 and 'S.' in p.text and len(p.text) > 40:
-                i += 1
-                print(f'{doc} {p.text}')
